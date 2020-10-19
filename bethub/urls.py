e175982a11ce6b14851dfcc0fbb9300d34826008
velinovasen <velinovasen@users.noreply.github.com>
2020-10-19 23:37:58 +0300

    'added_home_butt'

--- bethub/urls.py
+++ bethub/urls.py
@@ -18,7 +18,7 @@ from django.urls import path, include
 from traffic import views
 
 urlpatterns = [
-    path('', views.landing_view),
+    path('', views.landing_view, name='landing_page'),
     path('admin/', admin.site.urls),
     path('traffic/', include('traffic.urls')),
     path('predictions/', views.predictions, name='predictions'),
