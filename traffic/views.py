65427490f20fadd31531c8399f88ae5bee3bba7d
velinovasen <velinovasen@users.noreply.github.com>
2020-11-16 11:30:15 +0200

    'fixed_traffic_table'

--- traffic/views.py
+++ traffic/views.py
@@ -11,7 +11,7 @@ def landing_view(request):
 
 def traffic_volume(request):
     all_bets = BetsVolume.objects.all()
-    context = {"all_bets": all_bets}
+    context = {"all_traffic_bets": all_bets}
     return render(request, 'games/bets_volume.html', context)
 
 
